de14c870454124cdd7ade6cd9a870b5ad9a81975
phillip3lx <phillip3lx@gmail.com>
2026-03-07 15:36:39 -0800

    Initial Setup with JavaFX

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 import javafx.application.Application;
 import ui.CookBookUI;
-
+// TODO more UI stuff
 public class Main {
     public static void main(String[] args) {
         Application.launch(CookBookUI.class, args);
