6eebdd3d5648fe93ac6f0708ab0112d21d2e13e0
Phillip Lakes <163882224+akoli-pilot@users.noreply.github.com>
2026-03-14 19:01:46 -0700

    Create Main class
    Initial application bootstrap class for the CookBook UI.

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import javafx.application.Application;
+import views.CookBookUI;
+
+/**
+ * Application bootstrap class.
+ */
+// TODO: Units
+// TODO: Remaining Ingredients
+// TODO: API Integration
+// TODO: UX: User Input (onClickListeners...)
+public class Main {
+    public static void main(String[] args) {
+        Application.launch(CookBookUI.class, args);
+    }
+}
